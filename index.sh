cc41c3c998f4cba312bc9f85d5ecb7cb744bbf2e
hiichloee <reta1iate@hotmail.com>
2024-09-13 17:41:19 +1000

    test2

--- index.sh
+++ index.sh
@@ -1,3 +1,3 @@
 # This is a shell script that simply prints Hello World.
 # When you push changes to this shell script GitHub Action automatically runs it.
-echo "Hello computer!"
+echo "Hello Computer!"
